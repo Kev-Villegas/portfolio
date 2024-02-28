f5e9d1f3cc58e6f7127804369ee48ec0b5fa666c
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-02-28 18:08:53 -0300

    Fixing some Header issues

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className='z-[999] relative '>
+    <header className='z-[999] relative'>
       <motion.div
         className='fixed top-0 left-1/2  h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
         initial={{ y: -100, x: '-50%', opacity: 0 }}
@@ -17,7 +17,7 @@ const Header = () => {
           {links.map((link) => (
             <motion.li
               key={link.hash}
-              className='h-3/4 flex items-center justify-center font-semibold'
+              className='h-3/4 flex items-center justify-center relative font-semibold'
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
