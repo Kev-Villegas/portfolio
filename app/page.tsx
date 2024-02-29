e0e787598c4e280aa32a8d4f3f70948d5f3ed9a5
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-02-29 11:49:37 -0300

    Adding the About component to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero';
+import About from '@/components/About';
 import SectionDivider from '@/components/SectionDivider';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className='flex flex-col items-center px-4'>
       <Hero />
       <SectionDivider />
+      <About />
     </main>
   );
 }
