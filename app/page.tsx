e8d059ba4a8a0b94d558a270539ea01ccff8ffc7
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-02-29 11:48:11 -0300

    Adding the SectionDivider component to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero';
+import SectionDivider from '@/components/SectionDivider';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center px-4'>
       <Hero />
+      <SectionDivider />
     </main>
   );
 }
