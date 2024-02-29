82539f75dfcb0b910ed4ad0eea3a98e9e24cefa4
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-02-29 12:20:26 -0300

    Updating the About component and adding some styles

--- components/About.tsx
+++ components/About.tsx
@@ -12,29 +12,31 @@ const About = () => {
       id='about'
     >
       <SectionHeading>About me</SectionHeading>
-      <p className='mb-3 font-secondary leading-[30px] '>
-        Currently pursuing my final year in a technical programming degree, I am
-        poised to embark on the next phase of my educational journey by pursuing
-        a Bachelor's degree in Computer Science. With nearly four years of
-        hands-on experience in web development, I have honed my skills and
-        expertise in crafting dynamic and responsive web applications. My
-        current focus revolves around mastering{' '}
-        <span className='font-medium'>Next.js</span> with{' '}
-        <span className='font-medium'>TypeScript</span> and{' '}
-        <span className='font-medium'>React</span>, aligning with the latest
+      <p className='mb-3 font-primary font-light leading-7'>
+        Currently pursuing my final year in a{' '}
+        <span className='font-semibold'>Technical Programming Degree</span>, I
+        am poised to embark on the next phase of my educational journey by
+        pursuing a{' '}
+        <span className='font-semibold'>
+          Bachelor's degree in Computer Science
+        </span>
+        . With nearly four years of hands-on experience in web development, I
+        have honed my skills and expertise in crafting dynamic and responsive
+        web applications. My core stack is focused on{' '}
+        <span className='font-semibold'>Next.js</span> with{' '}
+        <span className='font-semibold'>TypeScript</span> and{' '}
+        <span className='font-semibold'>React</span>, aligning with the latest
         industry standards and best practices. Additionally, I am actively
-        delving into backend development, with aspirations to transition into a
-        versatile <span className='font-medium'>Full-Stack Developer</span>.
+        delving into backend development, with aspirations to transition into a{' '}
+        <span className='font-semibold'>Full-Stack Developer</span>.
       </p>
-
       <p className='font-secondary'>
-        <span className='italic'>Beyond the code,</span> I am fueled by a
-        passion for continuous learning and personal growth. Whether I'm
-        exploring the intricacies of backend technologies or delving into the
-        depths of computer science concepts, I thrive in challenging
-        environments that foster innovation and creativity. With each new
-        endeavor, I strive to expand my horizons and contribute meaningfully to
-        the ever-evolving field of technology.
+        <span className='italic'>
+          Passionate about continuous learning and personal growth,
+        </span>{' '}
+        I thrive in challenging environments that foster innovation and
+        creativity, constantly seeking to expand my horizons and contribute
+        meaningfully to the ever-evolving field of technology.
       </p>
     </motion.div>
   );
