a951ec6ad092662af0aff08295eed4b83bb097d9
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-01 11:45:59 -0300

    Building Project component

---
+++ components/Projects.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import Project from './Project';
+import { projectsData } from '@/lib/data';
+import SectionHeading from './SectionHeading';
+
+const Projects = () => {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
