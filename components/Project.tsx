6da2f86de99625c31ea0f5f90e1219d915d45a5e
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-04 11:00:03 -0300

    Updating some styles classes

--- components/Project.tsx
+++ components/Project.tsx
@@ -25,7 +25,7 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
       }}
       className='group mb-3 sm:mb-8 last:mb-0'
     >
-      <section className='max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20'>
+      <section className='max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 bg-white/10 hover:bg-white/20'>
         <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
           <h3 className='text-2xl font-semibold font-primary text-[#FFFFFF0] '>
             {title}
@@ -37,8 +37,7 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
             {tags.map((tag, index) => (
               <li
                 key={index}
-                // className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                className='bg-gray-300 font-tertiary font-semibold text-gray-800 px-3 py-2 text-[0.7rem] uppercase tracking-wider rounded-full cursor-pointer'
+                className='bg-gray-300 font-tertiary font-semibold text-[#222831] px-3 py-2 text-[0.7rem] uppercase tracking-wider rounded-full cursor-pointer'
               >
                 {tag}
               </li>
