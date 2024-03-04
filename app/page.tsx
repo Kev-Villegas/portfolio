2cc976d68a081bf308af4ad53e364d27d2729037
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-04 09:46:17 -0300

    Added Skills component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import SectionDivider from '@/components/SectionDivider';
 import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }
