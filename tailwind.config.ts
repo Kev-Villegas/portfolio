5e2f12d9c6f0bc15fabd3b1ea5c1efd9a3a78060
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-06 19:25:39 -0300

    Updating custom breakpoints in tailwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,9 @@ const config: Config = {
     screens: {
       sm: '640px',
       md: '768px',
-      lg: '960px',
-      xl: '1200px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     extend: {
       fontFamily: {
