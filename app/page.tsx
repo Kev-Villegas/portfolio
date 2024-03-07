9cb523b183a2ffdd817b3ecc1be002f0dd623a10
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-07 15:32:41 -0300

    Adding the Experience component to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Hero from '@/components/Hero';
 import About from '@/components/About';
-import SectionDivider from '@/components/SectionDivider';
-import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
+import Experience from '@/components/Experience';
+import SectionDivider from '@/components/SectionDivider';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
     </main>
   );
 }
