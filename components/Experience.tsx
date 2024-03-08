23b94b75119bdd752199502b3a6bb448466ace39
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-08 14:42:43 -0300

    Fixing some responsive issues and adding icons

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,41 +1,40 @@
 'use client';
 import React from 'react';
-import SectionHeading from '@/components/SectionHeading';
-import { experiencesData } from '@/lib/data';
-import { useSectionInView } from '@/lib/hooks';
-
 import Timeline from '@mui/lab/Timeline';
+import { experiencesData } from '@/lib/data';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { useSectionInView } from '@/lib/hooks';
 import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineContent from '@mui/lab/TimelineContent';
+import SectionHeading from '@/components/SectionHeading';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 
 const Experience = () => {
   const { ref } = useSectionInView('Experience');
+
   return (
-    <section ref={ref} id='experience' className="scroll-mt-28 mb-28 sm:mb-40">
+    <section ref={ref} id='experience' className='scroll-mt-28'>
       <SectionHeading>My Experience</SectionHeading>
       <Timeline position='alternate'>
         {experiencesData.map((item, index) => (
           <TimelineItem key={index}>
             {item.date && (
-              <TimelineOppositeContent className='text-[#ccc]'>
+              <TimelineOppositeContent className='text-lg mt-3'>
                 {item.date}
               </TimelineOppositeContent>
             )}
             <TimelineSeparator>
-              <TimelineDot />
+              <TimelineDot className='text-[#000] rounded-full border-[0.14rem] border-white shadow-xl text-2xl'>
+                {item.icon}
+              </TimelineDot>
+
               {index !== experiencesData.length - 1 && <TimelineConnector />}
             </TimelineSeparator>
             <TimelineContent>
-              <h3 className='font-medium capitalize tracking-wider font-primary text-[#fff] '>
-                {item.title}
-              </h3>
-              <p className='font-normal !mt-2 font-secondary tracking-wide text-[#ccc] '>
-                {item.description}
-              </p>
+              <h3 className='font-semibold capitalize'>{item.title}</h3>
+              <p className='font-normal !mt-0'>{item.description}</p>
             </TimelineContent>
           </TimelineItem>
         ))}
