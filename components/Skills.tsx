1453d992a725f115ef2f0137d0390d72459d34ed
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-09 14:43:37 -0300

    Adding new styles and fixing some colors

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -32,7 +32,7 @@ const Skills = () => {
       <ul className='flex flex-wrap justify-center gap-3 text-lg text-gray-800'>
         {skillsData.map((skill, index) => (
           <motion.li
-            className='bg-[#ececec] text-[#222831] font-medium borderBlack rounded-xl px-4 py-2 font-primary'
+            className='bg-[#383d46] text-base text-[#ececec] font-medium borderBlack rounded-xl px-4 py-2 font-primary'
             key={index}
             variants={fadeInAnimationVariants}
             initial='initial'
