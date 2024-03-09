6f013511f0278a9aa0f622d2da702fcefddcee89
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-09 14:20:19 -0300

    Adding new styles to the timeline

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -9,36 +9,46 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import SectionHeading from '@/components/SectionHeading';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 
 const Experience = () => {
-  const { ref } = useSectionInView('Experience');
+  const { ref } = useSectionInView('Projects', 0.5);
 
   return (
-    <section ref={ref} id='experience' className='scroll-mt-28'>
+    <section ref={ref} id='experience' className='scroll-mt-28 mb-28 sm:mb-40'>
       <SectionHeading>My Experience</SectionHeading>
-      <Timeline position='alternate'>
-        {experiencesData.map((item, index) => (
-          <TimelineItem key={index}>
-            {item.date && (
-              <TimelineOppositeContent className='text-lg mt-3'>
-                {item.date}
-              </TimelineOppositeContent>
-            )}
-            <TimelineSeparator>
-              <TimelineDot className='text-[#000] rounded-full border-[0.14rem] border-white shadow-xl text-2xl'>
-                {item.icon}
-              </TimelineDot>
+      <motion.div>
+        <Timeline position='alternate'>
+          {experiencesData.map((item, index) => (
+            <TimelineItem key={index}>
+              {item.date && (
+                <TimelineOppositeContent className='text-lg mt-[14px] font-secondary font-semibold'>
+                  {item.date}
+                </TimelineOppositeContent>
+              )}
+              <TimelineSeparator>
+                <TimelineDot
+                  color='primary'
+                  className='bg-opacity-100 border-[0.12rem] py-2 px-2 border-white text-2xl text-center justify-center'
+                >
+                  {item.icon}
+                </TimelineDot>
 
-              {index !== experiencesData.length - 1 && <TimelineConnector />}
-            </TimelineSeparator>
-            <TimelineContent>
-              <h3 className='font-semibold capitalize'>{item.title}</h3>
-              <p className='font-normal !mt-0'>{item.description}</p>
-            </TimelineContent>
-          </TimelineItem>
-        ))}
-      </Timeline>
+                {index !== experiencesData.length - 1 && <TimelineConnector />}
+              </TimelineSeparator>
+              <TimelineContent>
+                <h3 className='font-semibold capitalize text-xl text-[#f0fff0]'>
+                  {item.title}
+                </h3>
+                <p className='font-normal !mt-1 text-base text-[#D3D3D3]'>
+                  {item.description}
+                </p>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+        </Timeline>
+      </motion.div>
     </section>
   );
 };
