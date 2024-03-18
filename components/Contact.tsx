d1f073446bd7b08acb460feaea0ae4a0c158a81c
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-18 12:10:50 -0300

    Adding animations on the form and fixing issues

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,27 +1,53 @@
 'use client';
 import React from 'react';
-import SectionHeading from './SectionHeading';
 import { motion } from 'framer-motion';
+import { toast } from 'react-hot-toast';
+import SubmitButton from './SubmitButton';
+import SectionHeading from './SectionHeading';
 import { useSectionInView } from '@/lib/hooks';
-import { FaPaperPlane } from 'react-icons/fa';
+import { sendEmail } from '@/actions/sendEmail';
 
 const Contact = () => {
   const { ref } = useSectionInView('Contact');
   return (
-    <section
+    <motion.section
       id='contact'
       ref={ref}
-      className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'
+      className='mb-20 sm:mb-28 w-[min(100%,38rem)]'
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
       <SectionHeading>Contact Me</SectionHeading>
-      <p className='text-gray-700 -mt-6 text-white/80'>
+      <p className='text-gray-700 -mt-3 text-white/80'>
         Please contact me directly at{' '}
         <a className='underline' href='mailto:example@gmail.com'>
           example@gmail.com
-        </a>
+        </a>{' '}
         or through this form.
       </p>
-      <form className='mt-5 flex flex-col text-black'>
+      <form
+        className='mt-10 flex flex-col dark:text-black'
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success('Email Sent Successfully!');
+        }}
+      >
         <input
           className='h-14 px-4 rounded-lg borderBlack bg-white bg-opacity-80 focus:bg-opacity-100 transition-all outline-none'
           name='senderEmail'
@@ -31,21 +57,15 @@ const Contact = () => {
           placeholder='Your email'
         />
         <textarea
-          className='h-52 my-3 rounded-lg borderBlack p-4b bg-white bg-opacity-80 focus:bg-opacity-100 transition-all outline-none'
+          className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
           name='message'
           placeholder='Your message'
           required
           maxLength={5000}
         />
-        <button
-          type='submit'
-          className='group flex items-center gap-2 h-[3rem] w-[8rem] text-center justify-center text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 bg-white bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65'
-        >
-          Submit
-          <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1' />
-        </button>
+        <SubmitButton />
       </form>
-    </section>
+    </motion.section>
   );
 };
 
