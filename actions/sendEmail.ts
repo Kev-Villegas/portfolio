744c1801079509315df10725f9cc993f4fa55454
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-03-21 10:21:18 -0300

    Using a Zod Schema & adding the name prop

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,45 +1,51 @@
 'use server';
+import { z } from 'zod';
 import React from 'react';
 import { Resend } from 'resend';
-import { validateString, getErrorMessage } from '@/lib/utils';
 import { ContactFormEmail } from '@/email/contact-form-email';
+import { ContactFormSchema, FormDataSchema } from '@/lib/schema';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+type Inputs = z.infer<typeof FormDataSchema>;
 
-export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get('senderEmail');
-  const message = formData.get('message');
+export async function addEntry(data: Inputs) {
+  const result = FormDataSchema.safeParse(data);
 
-  if (!validateString(senderEmail, 500)) {
-    return {
-      error: 'Invalid sender email',
-    };
-  }
-  if (!validateString(message, 5000)) {
-    return {
-      error: 'Invalid message',
-    };
+  if (result.success) {
+    return { success: true, data: result.data };
   }
 
-  let data;
-  try {
-    data = await resend.emails.send({
-      from: 'Contact Form <onboarding@resend.dev>',
-      to: 'kevin.villegas1973@gmail.com',
-      subject: 'Message from contact form',
-      reply_to: senderEmail,
-      react: React.createElement(ContactFormEmail, {
-        message: message,
-        senderEmail: senderEmail,
-      }),
-    });
-  } catch (error: unknown) {
-    return {
-      error: getErrorMessage(error),
-    };
+  if (result.error) {
+    return { success: false, error: result.error.format() };
+  }
+}
+
+type ContactFormInputs = z.infer<typeof ContactFormSchema>;
+
+export async function sendEmail(data: ContactFormInputs) {
+  const result = ContactFormSchema.safeParse(data);
+
+  if (result.success) {
+    const { name, email, message } = result.data;
+    try {
+      const data = await resend.emails.send({
+        from: 'Contact Form <onboarding@resend.dev>',
+        to: 'kevin.villegas1973@gmail.com',
+        subject: 'Message from contact form',
+        reply_to: email,
+        react: React.createElement(ContactFormEmail, {
+          name: name,
+          message: message,
+          email: email,
+        }),
+      });
+      return { success: true, data };
+    } catch (error) {
+      return { success: false, error };
+    }
   }
 
-  return {
-    data,
-  };
-};
+  if (result.error) {
+    return { success: false, error: result.error.format() };
+  }
+}
