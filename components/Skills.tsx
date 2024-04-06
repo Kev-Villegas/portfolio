5da29d9b43c89106628af417aaa3256e5da74115
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-04-06 12:31:06 -0300

    Adding technology icons

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,12 @@
 'use client';
 import React from 'react';
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { skillsData } from '@/lib/data';
 import SectionHeading from './SectionHeading';
 import { useSectionInView } from '@/lib/hooks';
+
+type skillsProps = (typeof skillsData)[number];
 
 const fadeInAnimationVariants = {
   initial: {
@@ -19,7 +22,7 @@ const fadeInAnimationVariants = {
   }),
 };
 
-const Skills = () => {
+const Skills = ({ name, image }: skillsProps) => {
   const { ref } = useSectionInView('Skills');
 
   return (
@@ -32,7 +35,7 @@ const Skills = () => {
       <ul className='flex flex-wrap justify-center gap-3 text-lg text-gray-800'>
         {skillsData.map((skill, index) => (
           <motion.li
-            className='bg-[#383d46] text-base text-[#ececec] font-medium borderBlack rounded-xl px-4 py-2 font-primary'
+            className='bg-[#383d46] text-base text-[#ececec] font-medium borderBlack rounded-xl px-4 py-2 font-primary flex text-center justify-center items-center '
             key={index}
             variants={fadeInAnimationVariants}
             initial='initial'
@@ -42,7 +45,13 @@ const Skills = () => {
             }}
             custom={index}
           >
-            {skill}
+            <Image
+              src={skill.image}
+              alt={skill.name}
+              quality={90}
+              className='w-[22px] h-[22px] mr-2 rounded-[4px]'
+            />
+            {skill.name}
           </motion.li>
         ))}
       </ul>
