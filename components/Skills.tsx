d692b5a3367d65ba3d6b9ca900e9a992ea80b42b
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-04-08 12:45:46 -0300

    Adding Hover Effect

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -34,25 +34,20 @@ const Skills = ({ name, image }: skillsProps) => {
       <SectionHeading>My Skills</SectionHeading>
       <ul className='flex flex-wrap justify-center gap-3 text-lg text-gray-800'>
         {skillsData.map((skill, index) => (
-          <motion.li
-            className='bg-[#383d46] text-base text-[#ececec] font-medium borderBlack rounded-xl px-4 py-2 font-primary flex text-center justify-center items-center '
+          <li
             key={index}
-            variants={fadeInAnimationVariants}
-            initial='initial'
-            whileInView='animate'
-            viewport={{
-              once: true,
-            }}
-            custom={index}
+            className='bg-[#383d46] text-base text-[#ececec] font-medium borderBlack rounded-xl px-2 font-primary flex text-center justify-center items-center cursor-pointer relative overflow-hidden transition-transform duration-300 transform-gpu hover:scale-110'
           >
-            <Image
-              src={skill.image}
-              alt={skill.name}
-              quality={90}
-              className='w-[22px] h-[22px] mr-2 rounded-[4px]'
-            />
-            {skill.name}
-          </motion.li>
+            <div className='transition-colors duration-300 p-2 rounded-lg flex items-center'>
+              <Image
+                src={skill.image}
+                alt={skill.name}
+                quality={90}
+                className='w-[22px] h-[22px] mr-2 rounded-[4px] cursor-pointer'
+              />
+              <span>{skill.name}</span>
+            </div>
+          </li>
         ))}
       </ul>
     </section>
