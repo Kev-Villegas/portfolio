c7276a9be60529efc14b2747c32f318a054ea86d
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-04-09 06:45:56 -0300

    Updating classes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -28,7 +28,7 @@ const Projects = () => {
   return (
     <section ref={ref} id='projects' className='scroll-mt-28 mb-28'>
       <SectionHeading>My Projects</SectionHeading>
-      <motion.div className='flex flex-row gap-2 mb-4'>
+      <motion.div className='flex flex-row gap-2 mb-4 justify-start'>
         <button
           type='button'
           className='py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
