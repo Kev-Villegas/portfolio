9a23f2fc3880db6796b2809411781f7b63139a63
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-15 14:55:12 -0300

    changue some text and comment the buttons to filter in the projects component

--- components/About.tsx
+++ components/About.tsx
@@ -1,45 +1,43 @@
-'use client';
-import React from 'react';
-import { motion } from 'framer-motion';
-import { fadeIn } from './utils/FadeIn';
-import SectionHeading from './SectionHeading';
-import { useSectionInView } from '@/lib/hooks';
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "./utils/FadeIn";
+import SectionHeading from "./SectionHeading";
+import { useSectionInView } from "@/lib/hooks";
 const About = () => {
-  const { ref } = useSectionInView('About');
+  const { ref } = useSectionInView("About");
 
   return (
     <motion.div
       ref={ref}
-      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      id='about'
+      id="about"
     >
       <SectionHeading>About me</SectionHeading>
       <motion.div
-        variants={fadeIn('left', 0.5)}
-        initial='hidden'
-        whileInView={'show'}
+        variants={fadeIn("left", 0.5)}
+        initial="hidden"
+        whileInView={"show"}
         viewport={{ once: true, amount: 0.3 }}
       >
-        <p className='mb-2 font-primary font-light leading-7'>
-          Currently pursuing my final year in a{' '}
-          <span className='font-semibold'>Technical Programming Degree</span>, I
+        <p className="mb-2 font-primary font-light leading-7">
+          Currently pursuing my final year in a{" "}
+          <span className="font-semibold">Technical Programming Degree</span>, I
           am poised to embark on the next phase of my educational journey by
-          pursuing a{' '}
-          <span className='font-semibold'>
-            Bachelor's degree in Computer Science
+          pursuing a{" "}
+          <span className="font-semibold">
+            Bachelor's degree in Computer Science.
           </span>
-          . With nearly four years of hands-on experience in web development, I
-          have honed my skills and expertise in crafting dynamic and responsive
-          web applications. My core stack is focused on{' '}
-          <span className='font-semibold'>Next.js</span> with{' '}
-          <span className='font-semibold'>TypeScript</span> and{' '}
-          <span className='font-semibold'>React</span>, aligning with the latest
+          My core stack is focused on{" "}
+          <span className="font-semibold">Next.js</span> with{" "}
+          <span className="font-semibold">TypeScript</span> and{" "}
+          <span className="font-semibold">React</span>, aligning with the latest
           industry standards and best practices. Additionally, I am actively
           delving into backend development, with aspirations to transition into
-          a <span className='font-semibold'>Full-Stack Developer</span>.
+          a <span className="font-semibold">Full-Stack Developer</span>.
         </p>
       </motion.div>
     </motion.div>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,13 @@
-'use client';
-import Project from './Project';
-import { motion } from 'framer-motion';
-import React, { useState } from 'react';
-import { projectsData } from '@/lib/data';
-import SectionHeading from './SectionHeading';
-import { useSectionInView } from '@/lib/hooks';
+"use client";
+import Project from "./Project";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { projectsData } from "@/lib/data";
+import SectionHeading from "./SectionHeading";
+import { useSectionInView } from "@/lib/hooks";
 
 const Projects = () => {
-  const { ref } = useSectionInView('Projects', 0.5);
+  const { ref } = useSectionInView("Projects", 0.5);
   const [projectType, setProjectType] = useState<string | null>(null);
 
   const filteredProjects = projectsData.filter((project) => {
@@ -26,9 +26,9 @@ const Projects = () => {
   };
 
   return (
-    <section ref={ref} id='projects' className='scroll-mt-28 mb-28'>
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My Projects</SectionHeading>
-      <motion.div className='flex flex-row gap-2 mb-4 justify-start'>
+      {/* <motion.div className='flex flex-row gap-2 mb-4 justify-start'>
         <button
           type='button'
           className='py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
@@ -59,7 +59,7 @@ const Projects = () => {
         >
           Full-Stack
         </button>
-      </motion.div>
+      </motion.div> */}
       <div>
         {filteredProjects.map((project, index) => (
           <React.Fragment key={index}>
