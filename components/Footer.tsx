a3537a7339be9be024752b853df6c201ef882e68
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-22 17:24:56 -0300

    updating footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
+    <footer className="mb-3 px-4 text-center text-gray-500">
+      <small className="block text-xs">
         &copy; 2024 Kevin. All rights reserved.
       </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built using 
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend.
-      </p>
     </footer>
   );
 }
